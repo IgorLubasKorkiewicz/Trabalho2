d9a1fce29acc583350c4801c9c9114810173e65e
IgorLubasKorkiewicz <2024314594@aluno.riogrande.ifrs.edu.br>
2025-03-24 09:52:53 -0300

    Add files via upload

---
+++ Exerc1/2025-03-24/js.js
@@ -0,0 +1,12 @@
+ function Egg()
+{   
+    document.getElementById("Fasgas").innerText = "Grande Danger";
+    document.getElementById("Maco").src = "img/nave.png";
+}
+function Elll()
+{
+    document.getElementById("Random").innerText = (Math.trunc(Math.random()*31)%2 == 1);
+    
+    document.getElementById("Jumpscare").src = "img/nave.png"
+    
+}
